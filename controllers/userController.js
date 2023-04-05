1091dcb02b7f2278f2b0df9e2b1619ee9398a30a
john tom <johntommokaa@gmail.com>
2023-04-06 00:01:46 +0100

    more

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,7 +17,7 @@ const register = asyncHandler(async(req,res)=>{
     await newUser.save().then(()=>{console.log(newUser)}).catch((err)=>{console.log(err)})
 })
 
-const login = asyncHandler(async(req,res)=>{
+const login = asyncHandler(async(req,res,next)=>{
     const {email,password} = req.body
     if(!email || !password) res.json({message:"please input all fields"});
     //query databse for the user
@@ -26,5 +26,28 @@ const login = asyncHandler(async(req,res)=>{
 
     const pwdCheck= await bcrypt.compare(password,foundUser.password)
     if(!pwdCheck) res.json({message:'email or password is not correct'})
-    jwt.sign()
+    
+    const accessToken = jwt.sign(foundUser.email,process.env.ACCESS_TOKEN)
+    next()
+
 })
+
+const verifyToken = (req,res,next)=>{
+    const bearerHeader = req.headers["Authorization"]
+    const bearerToken = bearerHeader.split(' ')[1]
+    const verifiedToken = jwt.verify(bearerToken,process.env.ACCESS_TOKEN,(err,user))
+    if(err) console.log(err) 
+    console.log(user)
+    req.user= user
+    next()
+}
+
+const grantAccess= async(req,res,next)=>{
+    verifyToken(req,res,next,()=>{
+        if(req.user.isAdmin=== true){
+            next()
+        }else(console.log('you are not an admin'))
+    })
+}
+
+module.exports = {register,login,verifyToken,grantAccess}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const userDB = require('../model/userDB')
+const {register,login,verifyToken,grantAccess}= require('../controllers/userController')
 
 
 router.get('/user')
